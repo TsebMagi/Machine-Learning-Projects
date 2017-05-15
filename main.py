a60209384333723b0d835ff907d66c5e2015e31f
Doug Whitley <dwhitley@pdx.edu>
2017-05-14 17:34:56 -0700

    setup project with printouts

--- main.py
+++ main.py
@@ -1,69 +1,32 @@
 import pandas as pd
-import random
 import sklearn.model_selection
-import sklearn.svm
-import sklearn.preprocessing
-
-
-def run_experiment1():
-    print("Experiment 1", '\n')
-    # Get the relevant stats
-    predictions = classifier.predict(x_test)
-    probabilities = classifier.predict_proba(x_test)
-    acc = classifier.score(x_test, y_test)
-    precision = sklearn.metrics.precision_score(y_test, predictions)
-    recall = sklearn.metrics.recall_score(y_test, predictions)
-    print("Accuracy", acc)
-    print("Precision", precision)
-    print("Recall", recall)
-    # Produce the data for the ROC curve
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_test, probabilities[:, 1], drop_intermediate=True)
-    for x in range(len(thresholds)):
-        print(fpr[x], ',', tpr[x])
-
-
-def run_experiment2():
-    print("Experiment 2", '\n')
-    # Setup an array of the indices of the features in increasing order of weight
-    temp = classifier.coef_
-    arr = temp[0].argsort()[-57:][::-1]
-    # Run through the features
-    for x in range(2, 58):
-        # Fit and calculate accuracy for the set of Features
-        classifier2 = sklearn.svm.SVC(kernel='linear')
-        classifier2.fit(x_train.iloc[:, arr[:x]], y_train)
-        sorted_acc = classifier2.score(x_test.iloc[:, arr[:x]], y_test)
-        print(x, ',', sorted_acc)
-
-
-def run_experiment3():
-    print("Experiment 3", '\n')
-    # Setup an array of random indices
-    arr = [x for x in range(57)]
-    random.shuffle(arr)
-    # Run through the features
-    for x in range(2, 58):
-        # Fit and calculate accuracy for the set of features
-        classifier3 = sklearn.svm.SVC(kernel='linear')
-        classifier3.fit(x_train.iloc[:, arr[:x]], y_train)
-        rand_acc = classifier3.score(x_test.iloc[:, arr[:x]], y_test)
-        print(x, ',', rand_acc)
-
+from math import sqrt, e, pi, log, exp, pow
+import numpy
 
 if __name__ == "__main__":
     # Load and process data in the manner that Mike prescribed
     data = pd.read_csv("spambase.data", header=None, index_col=57)
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(data, data.index.values, test_size=0.5)
-    scaler = sklearn.preprocessing.StandardScaler().fit(x_train)
+    # Setup base probabilities
+    p_spam = sum(y_train) / len(y_train)
+    p_not_spam = 1 - p_spam
 
-    # Transform the training data based on that scaler and set the index values accordingly
-    x_train = pd.DataFrame(scaler.transform(x_train), index=y_train)
-
-    # Transform the testing data based on that same scaler and set the index values accordingly
-    x_test = pd.DataFrame(scaler.transform(x_test), index=y_test)
-    classifier = sklearn.svm.SVC(kernel='linear', probability=True)
-    classifier.fit(x_train, y_train)
-    # Run the experiments
-    run_experiment1()
-    run_experiment2()
-    run_experiment3()
+    # Calculate standard deviations and means for the training features
+    std_spam = x_train.loc[1].std(axis=0).replace(0, 0.0001).as_matrix()
+    std_not_spam = x_train.loc[0].std(axis=0).replace(0, 0.0001).as_matrix()
+    mean_spam = x_train.loc[1].mean(axis=0).as_matrix()
+    mean_not_spam = x_train.loc[0].mean(axis=0).as_matrix()
+    # Gauss 1/(sqrt(2*pi)*s)*e**(-0.5*(float(x-m)/s)**2)
+    print(x_train)
+    print(std_spam)
+    print(std_not_spam)
+    print(mean_spam)
+    print(mean_not_spam)
+    print(len(x_test), len(x_train))
+    for row in range(len(x_test)):
+        spam_class = log(p_spam)
+        for x in range(57):
+            p_spam += log(1 / (sqrt(2 * pi) * std_spam[x]) * e ** (
+            -0.5 * (float(x_test.iloc[row, x] - mean_spam[x]) / std_spam[x]) ** 2))
+        pass
+        # Calculate parts of the Gauss equation
