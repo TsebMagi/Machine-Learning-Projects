82e028a87e8a4d1aa7ef22059ffb2abfcccadd21
Doug Whitley <dwhitley@pdx.edu>
2017-05-16 10:46:53 -0700

    Working Project 4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sklearn.model_selection
-from math import sqrt, e, pi, log, exp, pow
+from math import sqrt, e, pi, log
 import numpy
 
 if __name__ == "__main__":
@@ -12,21 +12,45 @@ if __name__ == "__main__":
     p_not_spam = 1 - p_spam
 
     # Calculate standard deviations and means for the training features
-    std_spam = x_train.loc[1].std(axis=0).replace(0, 0.0001).as_matrix()
-    std_not_spam = x_train.loc[0].std(axis=0).replace(0, 0.0001).as_matrix()
-    mean_spam = x_train.loc[1].mean(axis=0).as_matrix()
-    mean_not_spam = x_train.loc[0].mean(axis=0).as_matrix()
-    # Gauss 1/(sqrt(2*pi)*s)*e**(-0.5*(float(x-m)/s)**2)
-    print(x_train)
-    print(std_spam)
-    print(std_not_spam)
-    print(mean_spam)
-    print(mean_not_spam)
-    print(len(x_test), len(x_train))
+    std_spam = x_train.loc[1].std(axis=0).replace(0, 0.0001)
+    std_not_spam = x_train.loc[0].std(axis=0).replace(0, 0.0001)
+    mean_spam = x_train.loc[1].mean(axis=0)
+    mean_not_spam = x_train.loc[0].mean(axis=0)
+    # Setup the tracker variables
+    correct = 0
+    confusion_matrix = pd.DataFrame(numpy.zeros((2, 2)))
+    # Go through the test set
     for row in range(len(x_test)):
+        # Setup the base probabilities
         spam_class = log(p_spam)
+        not_spam_class = log(p_not_spam)
+        # Calculate each Feature
         for x in range(57):
-            p_spam += log(1 / (sqrt(2 * pi) * std_spam[x]) * e ** (
-            -0.5 * (float(x_test.iloc[row, x] - mean_spam[x]) / std_spam[x]) ** 2))
-        pass
-        # Calculate parts of the Gauss equation
+            # Split the log term into two via the log addition property
+            spam_class += log(1 / (sqrt(2 * pi) * std_spam[x]))
+            # Calculate second term separately to avoid taking log of 0
+            second_term_spam = e ** (-0.5 * (float(x_test.iloc[row, x] - mean_spam[x]) / std_spam[x]) ** 2)
+            # if second term has gone to 0 use very negative number instead
+            if second_term_spam == 0:
+                spam_class += -100000000
+            else:
+                # Didn't go to zero
+                spam_class += log(second_term_spam)
+            # Repeat above for the not spam case
+            not_spam_class += log(1 / (sqrt(2 * pi) * std_not_spam[x]))
+            second_term_not_spam = e ** (-0.5 * (float(x_test.iloc[row, x] - mean_not_spam[x]) / std_not_spam[x]) ** 2)
+            if second_term_not_spam == 0:
+                not_spam_class += -100000000
+            else:
+                not_spam_class += log(second_term_not_spam)
+        # Calculate guess and update stats
+        if spam_class > not_spam_class:
+            guess = 1
+        else:
+            guess = 0
+        if guess == y_test[row]:
+            correct += 1
+        confusion_matrix.iloc[guess, y_test[row]] += 1
+    # Display results
+    print("Accuracy", correct / len(x_train))
+    print(confusion_matrix)
