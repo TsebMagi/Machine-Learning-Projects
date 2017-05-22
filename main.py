169a7635d74602c689fa81d655bbca78f6d87412
Doug Whitley <dwhitley@pdx.edu>
2017-05-22 14:37:51 -0700

    work on initial k_mean algorithm

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np
 
+
+def create_clusters(data, size):
+    return np.random.choice(data, size, replace=False)
+
+
+def run_k_means(data, clusters, ):
+    pass
+
+
 if __name__ == "__main__":
     # load data
-    data = pd.read_csv("optdigits-train.txt", sep=',', index_col=-1, dtype=np.float64, header=None)
-
-    print(data)
+    train_data = pd.read_csv("optdigits-train.txt", sep=',', index_col=-1, dtype=np.float64, header=None)
+    test_data = pd.read_csv("optdigits-test.txt", sep=',', index_col=-1, dtype=np.float64, header=None)
