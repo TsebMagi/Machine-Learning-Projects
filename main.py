d893fde18fc39db9eeef334f8e5ceae742a83f6f
Doug Whitley <dwhitley@pdx.edu>
2017-05-22 14:16:17 -0700

    Project 5 Init

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import sklearn.model_selection
-from math import sqrt, e, pi, log
-import numpy
+import numpy as np
 
 if __name__ == "__main__":
+    # load data
+    data = pd.read_csv("optdigits-train.txt", sep=',', index_col=-1, dtype=np.float64, header=None)
+
+    print(data)
